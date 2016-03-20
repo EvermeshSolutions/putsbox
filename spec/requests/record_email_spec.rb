9631c1507274dd164c971841d20fd589d76c2b97
Pablo Cantero <pablo@pablocantero.com>
2016-03-20 17:50:11 -0300

    Fix specs

--- spec/requests/record_email_spec.rb
+++ spec/requests/record_email_spec.rb
@@ -1,26 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe 'Record Email', type: :request do
-  let(:mandrill_events) do
-    [{
-      event: 'inbound',
-      ts: Time.now.to_i,
-      msg: {
-        headers: [],
-        from_email: 'from@example.com',
-        from_name: 'John',
-        to: [['to@example.com', nil]],
-        email: 'to@example.com',
-        subject: 'Hello',
-        text: 'Lorem Ipsum',
-        html: '<p>Lorem Ipsum</p>'
-      }
-    }.with_indifferent_access]
+  let(:inbound_params) do
+    {
+      headers: '...',
+      from: 'John <from@example.com>',
+      to: ['to@putsbox.com'],
+      subject: 'Hello',
+      text: 'Lorem Ipsum',
+      html: '<p>Lorem Ipsum</p>',
+      envelope: { to: ['to@putsbox.com'], from: 'from@example.com' }.to_json
+    }.with_indifferent_access
   end
 
   it 'creates a new bucket' do
     expect {
-      post '/record', mandrill_events: JSON.dump(mandrill_events)
+      post '/record', inbound_params
     }.to change(Bucket, :count).by(1)
 
     expect(response).to be_ok
@@ -31,7 +26,7 @@ RSpec.describe 'Record Email', type: :request do
 
     it 'updates a bucket' do
       expect {
-        post '/record', mandrill_events: JSON.dump(mandrill_events)
+        post '/record', inbound_params
       }.to change(Bucket, :count).by(0)
 
       expect(bucket.reload.emails_count).to eq(1)
