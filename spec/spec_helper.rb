27045bb6cf5c34b79c407e67f502128279ae67be
Pablo Cantero <pablo@pablocantero.com>
2017-06-18 12:33:19 -0400

    Allow codeclimate.com spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,27 +39,9 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 Mongo::Logger.logger.level = ::Logger::FATAL
 
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
+
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = 'random'
 
   config.before do
