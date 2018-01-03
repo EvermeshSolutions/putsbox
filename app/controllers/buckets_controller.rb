6c3b309b2187d522dbb4d416bf2bddb005e95fbf
Pablo Cantero <pablo@pablocantero.com>
2018-01-02 20:06:07 -0500

    MOAR encoding attempts
    Fix https://rollbar.com/putsbox/putsbox/items/15
    Fix https://rollbar.com/putsbox/putsbox/items/14
    
    See https://stackoverflow.com/a/17023810/464685

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -43,17 +43,35 @@ class BucketsController < ApplicationController
       email_params['attachments'] = JSON.parse(params['attachment-info']).values
     end
 
-    email_params = email_params.permit(:headers, :from_email, :from_name, :subject, :text, :html, :subject, :email, to: [], attachments: [:filename, :name, :type])
+    email_params = email_params.permit(
+      :headers,
+      :from_email,
+      :from_name,
+      :subject,
+      :text,
+      :html,
+      :subject,
+      :email,
+      :charsets,
+      to: [],
+      attachments: [:filename, :name, :type]
+    )
 
     # See https://rollbar.com/putsbox/putsbox/items/15
     # request.POST.charsets	{"to":"UTF-8","html":"us-ascii","subject":"UTF-8","from":"UTF-8","text":"us-ascii"}
-    email_params[:text] = email_params[:text].encode!(Encoding::UTF_8) if email_params[:text]
-    email_params[:html] = email_params[:html].encode!(Encoding::UTF_8) if email_params[:html]
+    set_encoding!(email_params, :text)
+    set_encoding!(email_params, :html)
 
     RecordEmail.call(token: email_params['email'].gsub(/\@.*/, ''),
                      email: Email.new(email_params),
                      request: request)
 
     head :ok
+  end
+
+  def set_encoding!(params, key)
+    return if params[key].nil? || params[:charsets].to_h[key].nil?
+
+    params[key] = params[key].encode(params[:charsets][key]).force_encoding(Encoding::UTF_8)
   end
 end
