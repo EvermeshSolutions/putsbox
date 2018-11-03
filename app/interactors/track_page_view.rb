1d3aa95a70de0b96f1ab16928cabd096fadff865
Pablo Cantero <pablohstc@gmail.com>
2018-11-03 09:17:50 -0400

    Rubocop :lipstick:

--- app/interactors/track_page_view.rb
+++ app/interactors/track_page_view.rb
@@ -5,12 +5,12 @@ class TrackPageView
     return unless ENV['GA']
 
     options = {
-      hostname:    request.host,
-      path:        request.path,
-      user_id:     request.session.id,
-      user_ip:     request.remote_ip,
-      user_agent:  request.user_agent,
-      referrer:    request.referer
+      hostname: request.host,
+      path: request.path,
+      user_id: request.session.id,
+      user_ip: request.remote_ip,
+      user_agent: request.user_agent,
+      referrer: request.referer
     }
 
     tracker = Staccato.tracker(ENV['GA'], nil, options)
